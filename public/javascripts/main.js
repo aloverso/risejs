082d909543c460af82d6596c328775e59f936d70
Anne LoVerso <aelv13@gmail.com>
2016-07-14 19:22:12 -0400

    refactor for readability

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -29,18 +29,11 @@ var CONFIG = {
     "contestName":undefined
 };
 
-
-function getNumSongs() {
-	return CONFIG.numsongs;
-}
-
 function Container() {
 
 	var currentSong;
-	var songs = []; // contains loading song objects
 	var counter = 0;
-	var start = true;
-	var notplaying = true;
+	var gamehasstarted = false;
 	var timesecs = CONFIG.playtime;
 	var score = 0;
 	var ended = false;
@@ -49,6 +42,7 @@ function Container() {
 	var timerint;
 	var songcheckint;
 
+	var songs = []; // contains loading song objects
 	var loads = []; // contains loading audio elements
 
 	var canplay = []; // contains loaded audio elements
@@ -74,8 +68,6 @@ function Container() {
 		return ended;
 	}
 
-	var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
 	function giveup() {
 		revealanswers();
 	}
@@ -93,28 +85,20 @@ function Container() {
 			var s = songs[i];
 			var audioElement = document.createElement('audio');
 			audioElement.setAttribute('src', s.path);
-			console.log(s.startTime);
 			audioElement.currentTime = s.startTime;
 			audioElement.load();
 			audioElement.setAttribute('id',s.title)
 			audioElement.preload = "auto";
 			audioElement.added = false;
 			audioElement.oncanplaythrough = function() {
-				// console.log("asfdasdf");
-				// console.log(canplay.indexOf(this));
-				//console.log(this.added);
-				//console.log(gameEnded());
-				//console.log(this);
 				if (canplay.indexOf(this) < 0 && !this.added && !gameEnded()) {
 					canplay.push(this);
 					t = this.getAttribute('id');
 
-					// find the actual song json object to push
 					for (var j=0; j<num; j++) {
 						if (songs[j].title === t) {
 							songsplay.push(songs[j]);
-							//console.log("woot2"+t);
-							console.log("woot3");
+							console.log("load");
 							this.added = true;
 							break;
 						}
@@ -123,7 +107,6 @@ function Container() {
 				}
 			}
 			loads.push(audioElement);
-			//console.log(loads);
 		}
 	}
 
@@ -139,22 +122,20 @@ function Container() {
 	}
 
 	function setgame() {
-
-		// make sure we don't have song repeats
-		
-		//console.log("DLSKFLKJ");
+		// reset elements
 		$('#giveup-button').css('display', 'inline');
 		$('#interaction-unit').css('display', 'block');
 		$('#contest-message').html('');
-		$('#score').html("Score: "+score);
+		$('#score').html("Score: "+score + " / "+CONFIG.numsongs);
 		$('#timer').css('color','#000');
 		$('#playpauser').attr('class', 'fa fa-pause-circle-o fa-5x clickable');
 
+		// only use required songs
 		canplay.splice(CONFIG.numsongs, 25);
 
+		// append any ready-to-play songs
 		for (var i=0; i<canplay.length; i++) {
 			var t = canplay[i].getAttribute('id');
-			//console.log(t);
 			$('#loading').remove();
 			$('#statusholder').append("<div class='status-box clickable' id='"+t.hashCode()+"' onclick='c.jumpsong("+i+")'>"+(i+1)+"</div>");
 			
@@ -163,42 +144,7 @@ function Container() {
 			}
 		}
 
-			
-		// 	data.startTime = Math.floor((data.duration-15) * Math.random());
-		// 	data.guess = false;
-		// 	console.log(songs);
-		// 	if (songs.indexOf(data)<=0) {
-		// 		songs.push(data);
-		// 		console.log('hella');
-		// 	}
-		// 	else {
-		// 		$.get("getnewsong")
-		// 		.done(function(data) {
-					
-		// 			data.startTime = Math.floor((data.duration-15) * Math.random());
-		// 			data.guess = false;
-		// 			if (songs.indexOf(data)<=0) {
-		// 				songs.push(data);
-		// 			}
-		// 			else {
-		// 				$.get("getnewsong")
-		// 				.done(function(data) {
-							
-		// 					data.startTime = Math.floor((data.duration-15) * Math.random());
-		// 					data.guess = false;
-		// 					if (songs.indexOf(data)<=0) {
-		// 						songs.push(data);
-		// 					}
-		// 					else {
-		// 						location.reload();
-		// 					}
-		// 			});
-		// 		}
-		// 	});
-		// }
-
-
-
+		// create 200ms interval to check for song reset time (and other functions)
 		songcheckint = setInterval(function() {
 			if (currentSong) {
 				if (canplay[counter].currentTime >= currentSong.startTime + parseInt(CONFIG.songlen)) {
@@ -206,107 +152,50 @@ function Container() {
 				}
 			}
 
-			//console.log(canplay.length);
-			//console.log($('#statusholder').children().length);
-
+			// as more songs load keep splicing
 			canplay.splice(CONFIG.numsongs, 25);
 
+			// add any more songs that have loaded to the statusholder
 			if (canplay.length > $('#statusholder').children().length) {
 				for (var i=($('#statusholder').children().length); i<canplay.length; i++) {
 
 					var t = canplay[i].getAttribute('id');
-					//console.log(t);
 					$('#loading').remove();
 					$('#statusholder').append("<div class='status-box clickable' id='"+t.hashCode()+"' onclick='c.jumpsong("+i+")'>"+(i+1)+"</div>");
 					
 					if (i+1 < CONFIG.numsongs) {
-						//console.log('holla'+i);
 						$('#loadingholder').append("<div id='loading'>LOADING...</div>");
 					}
 				}
 			}
 
-
 			// if guessed all (loaded) songs
 			if (guesses >= canplay.length) {
-				// ended = true;
-				// pausesong();
-				// $('#interaction-unit').css('display', 'none');
-				// $('#loading').remove();
 				revealanswers();
 			}
 
-				// first time
-				//**********************
-				if ((start || notplaying) && songs.length >= CONFIG.numsongs) {
-
-					if (start) {
-
-						// for (var i=0; i<CONFIG.numsongs; i++) {
-							
-						// }
-
-						
-
-						// for (var i=0; i<numsongs; i++) {
-						// 	console.log("aksdf");
-						// 	var audioElement = document.createElement('audio');
-						// 	audioElement.setAttribute('src', songs[i].path);
-						// 	audioElement.currentTime = songs[i].startTime;
-						// 	audioElement.load();
-						// 	audioElement.setAttribute('id',songs[i].title)
-						// 	audioElement.preload = "auto";
-						// 	audioElement.added = false;
-						// 	audioElement.oncanplaythrough = function() {
-						// 		if (canplay.indexOf(this) < 0 && !this.added && !gameEnded()) {
-						// 			console.log("woott");
-						// 			canplay.push(this);
-						// 			console.log("woottt");
-						// 			t = this.getAttribute('id');
-						// 			console.log("woot"+t);
-						// 			for (var j=0; j<numsongs; j++) {
-						// 				if (songs[j].title === t) {
-						// 					songsplay.push(songs[j]);
-						// 					console.log("woot2"+t);
-						// 					break;
-						// 				}
-						// 			}
-						// 			console.log("woot3"+t);
-						// 			$('#loading').remove();
-						// 			$('#status').append("<div class='status-box clickable' id='"+t.hashCode()+"' onclick='c.jumpsong("+(canplay.indexOf(this))+")'>"+(canplay.indexOf(this)+1)+"</div>");
-						// 			if (canplay.length < numsongs) {
-						// 				$('#status').append("<div id='loading'>LOADING...</div>");
-						// 			}
-						// 			this.added = true;
-						// 		}
-						// 	}
-						// 	loads.push(audioElement);
-						// 	console.log(loads);
-						// }
-						// start = false;
-					}
-
-					if (canplay.length > 0) {
-						// console.log(canplay);
-						currentSong = songsplay[0];
-						canplay[0].currentTime = currentSong.startTime;
-						if(CONFIG.albumart) {
-							$('#albumart').attr('src', 'images/'+currentSong.album+'.jpg');
-						} else {
-							$('#albumart').attr('src', 'images/noart.jpg');
-						}
-						$('#'+currentSong.title.hashCode()).css('border', '3px solid #333');
-						canplay[0].play();
-						notplaying = false;
-						start = false;
-					}
-
+			// first time - wait until we can play at least one song (but only execute this once)
+			//**********************
+			if (canplay.length > 0 && !gamehasstarted) {
+				// console.log(canplay);
+				currentSong = songsplay[0];
+				canplay[0].currentTime = currentSong.startTime;
+				if(CONFIG.albumart) {
+					$('#albumart').attr('src', 'images/'+currentSong.album+'.jpg');
+				} else {
+					$('#albumart').attr('src', 'images/noart.jpg');
 				}
+				$('#'+currentSong.title.hashCode()).css('border', '3px solid #333');
+				canplay[0].play();
+				gamehasstarted = true;
+			}
+
 		}, 200);
 
+		// set interval to increment timer
 		timerint = setInterval(function() {
-			// gameplay = 1, time decreasing
-			if (CONFIG.gameplay && !notplaying && !ended) {
+			// if we're playing count-down game
+			if (CONFIG.gameplay && gamehasstarted && !ended) {
 				timesecs -= 1;
 				if (timesecs <= 15) {
 					$('#timer').css('color','#fe2c3b');
@@ -318,13 +207,12 @@ function Container() {
 				} else {
 					$('#timer').html("Time: "+formatSecString(timesecs));
 				}
-			} else if (!ended && !notplaying) {
+			} else if (gamehasstarted && !ended) { // count-up game
 				timesecs += 1;
 				$('#timer').html("Time: "+formatSecString(timesecs));
 			}
-
-			console.log(canplay[counter].currentTime);
-			console.log(canplay[counter].paused);
+			// console.log(canplay[counter].currentTime);
+			// console.log(canplay[counter].paused);
 
 		}, 1000);
 	}
@@ -335,6 +223,7 @@ function Container() {
 		$('#loading').remove();
 		$('#giveup-button').css('display', 'none');
 
+		// set unguessed status blocks to red and show title
 		for(var i=0; i<canplay.length; i++) {
 			var s = songsplay[i];
 			if (!s.guess) {
@@ -346,7 +235,7 @@ function Container() {
 
 		ended = true;
 
-		console.log(CONFIG.contestName, CONFIG.username, notsaved);
+		// if playing a contest, save score and show place
 		if (CONFIG.contestName && CONFIG.username && notsaved) {
 			console.log("in it");
 			$.post('/saveEntry', {
@@ -355,7 +244,6 @@ function Container() {
 				'score': score,
 				'timesecs': CONFIG.playtime - timesecs 
 			}).success(function(data) {
-				// console.log(data);
 				notsaved = false;
 				$('#contest-message').html(makeContestMessage(data.place, data.total));
 			});
@@ -417,9 +305,6 @@ function Container() {
 			$('#'+currentSong.title.hashCode()).css('border', '0px solid #333');
 			currentSong = song;
 			$('#'+currentSong.title.hashCode()).css('border', '3px solid #333');
-			if (CONFIG.albumart) {
-				$('#albumart').attr('src', '');
-			}
 		
 			canplay[counter].currentTime = currentSong.startTime;
 			canplay[counter].play();
@@ -430,6 +315,7 @@ function Container() {
 		}
 	}
 
+	// switch back to setup mode
 	function newgame() {
 		$('#GAME').css('display','none');
 		$('#SETUP').css('display','block');
@@ -497,7 +383,7 @@ function Container() {
 
 			if (amendguess(strip(guess)) === strip(currentSong.title)) {
 				score += 1;
-				$('#score').html("Score: "+score);
+				$('#score').html("Score: "+score + " / " + CONFIG.numsongs);
 				$('#'+currentSong.title.hashCode()).html("<i class='fa fa-check-circle'></i> "+currentSong.title);
 				$('#'+currentSong.title.hashCode()).css('background', '#00eb34');
 				nextsong();
@@ -510,24 +396,6 @@ function Container() {
 		}
 		return false;
 	}
-}
-
-var c = null;
-var savedPlaytime;
-
-
-function GAME() {
-
-	// $.get('/getConfig').done(function(data) {
-
-		// FIXED NUMBER OF SONGS
-		// if (CONFIG.gameplay === 0) {
-		c.settime(CONFIG.playtime);
-		c.setgame();
-		// }
-	// });
-
-	var $guessForm = $('form.guessform').unbind();
 }
 
 function submitGuess(event) {
@@ -548,12 +416,10 @@ function formatSecString(secs) {
 	    return mins+"m"+se+"s";
 	}
 
-function fixedtime() {
+function timelimit() {
 	CONFIG.gameplay = 1;
-	//console.log(CONFIG);
-	$('#fixedtime').attr('class','button-primary');
-	$('#fixednum').removeClass('button-primary');
-
+	$('#timelimit').attr('class','button-primary');
+	$('#infinitetime').removeClass('button-primary');
 	$('#allotted').css('display', 'block');
 	$('#playtime').val(savedPlaytime);
 	$('#playtime-res').text(formatSecString(savedPlaytime));
@@ -562,11 +428,10 @@ function fixedtime() {
 	return false;
 }
 
-function fixednum () {
+function infinitetime () {
 	CONFIG.gameplay = 0;
-	//console.log(CONFIG);
-	$('#fixednum').attr('class','button-primary');
-	$('#fixedtime').removeClass('button-primary');
+	$('#infinitetime').attr('class','button-primary');
+	$('#timelimit').removeClass('button-primary');
 	CONFIG.playtime = 0;
 	$('#allotted').css('display', 'none');
 
@@ -574,41 +439,32 @@ function fixednum () {
 }
 
 function startgame() {
-	// localStorage.setItem("conf", JSON.stringify(CONFIG));
-	// console.log(JSON.parse(localStorage.getItem("conf")));
-	// console.log(CONFIG);
-	// $.post('/updateConfig', CONFIG).success(
-	// 	$(location).attr('href', '/game')
-	// );
 	$('#SETUP').css('display','none');
 	$('#GAME').css('display','block');
 	
-	GAME();
+	c.settime(CONFIG.playtime);
+	c.setgame();
+
+	var $guessForm = $('form.guessform').unbind();
+}
+
+function setSlidersToConfigs() {
+	$('#numsongs-res').text(CONFIG.numsongs);
+	$('#numsongs').val(CONFIG.numsongs);
+	$('#songlen-res').text(CONFIG.songlen);
+	$('#songlen').val(CONFIG.songlen);
+	$('#playtime-res').text(CONFIG.playtime);
+	$('#playtime').val(CONFIG.playtime);
+	$('#myonoffswitch')[0].checked = CONFIG.albumart ? true : false;
 }
 
 function reset() {
-	// $.get('/reset').done(function(data) {
-		CONFIG = defaultconfig;
-		$('#numsongs-res').text(CONFIG.numsongs);
-		$('#numsongs').val(CONFIG.numsongs);
-		$('#songlen-res').text(CONFIG.songlen);
-		$('#songlen').val(CONFIG.songlen);
-		$('#playtime-res').text(CONFIG.playtime);
-		$('#playtime').val(CONFIG.playtime);
-		$('#myonoffswitch')[0].checked = CONFIG.albumart ? true : false;
+	CONFIG = defaultconfig;
+	setSlidersToConfigs();
 
-		if (CONFIG.playtime===0) {
-			savedPlaytime = 180;
-		} else {
-			savedPlaytime = CONFIG.playtime;
-		}
+	savedPlaytime = CONFIG.playtime;
 
-		if (CONFIG.gameplay===1) {
-			fixedtime();
-		} else {
-			fixednum();
-		}
-	// });
+	timelimit();
 }
 
 function SETUP() {
@@ -617,36 +473,25 @@ function SETUP() {
 	c = new Container();
 	c.STARTLOADING(25);
 
-	// $.get('/getConfig').done(function(data) {
-	// 	CONFIG = data;
-		$('#numsongs-res').text(CONFIG.numsongs);
-		$('#numsongs').val(CONFIG.numsongs);
-		$('#songlen-res').text(CONFIG.songlen);
-		$('#songlen').val(CONFIG.songlen);
-		$('#playtime-res').text(CONFIG.playtime);
-		$('#playtime').val(CONFIG.playtime);
-		//console.log($('#myonoffswitch')[0]);
-		$('#myonoffswitch')[0].checked = CONFIG.albumart ? true : false;
+	setSlidersToConfigs();
 
-		if (CONFIG.playtime===0) {
-			savedPlaytime = 180;
-		} else {
-			savedPlaytime = CONFIG.playtime;
-		}
+	if (CONFIG.playtime===0) {
+		savedPlaytime = 180;
+	} else {
+		savedPlaytime = CONFIG.playtime;
+	}
 
-		if (CONFIG.gameplay===1) {
-			fixedtime();
-		} else {
-			fixednum();
-		}
-	// });
+	if (CONFIG.gameplay===1) {
+		timelimit();
+	} else {
+		infinitetime();
+	}
 
 	populateContest('extreme');
 	populateContest('ordinary');
 
 
 	$('#myonoffswitch').change(function() {
-		//console.log("HOLLA");
 		if(this.checked) {
 			CONFIG.albumart = 1;
 		} else {
@@ -654,13 +499,9 @@ function SETUP() {
 		}
 	});
 
-	// Initialize a new plugin instance for all
-	// e.g. $('input[type="range"]') elements.
-
     $('input[type="range"]').rangeslider();
 
 	$('input').on('input', function () {
-		// console.log("WKASLFD");
 		CONFIG[$(this).attr('id')] = parseInt($(this).val());
 		
 		if($(this).attr('id') === 'playtime') {
@@ -678,7 +519,6 @@ function SETUP() {
 function populateContest(id) {
 	$.get("/getEntries", {'contest': id, 'number': 10})
   	.done(function(data) {
-  		// console.log(data);
   		$list = $('#'+id);
   		for (var i=0; i<10; i++) {
   			if(data[i])	$list.append(makeEntry(data[i].name, data[i].timesecs, data[i].score));
@@ -721,5 +561,8 @@ function join(contest) {
 		startgame();
 	}
 }
+
+var c = null;
+var savedPlaytime;
 
 SETUP();
